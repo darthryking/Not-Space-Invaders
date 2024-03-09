97f619908b014907510cf42ca4a7ed62d9931bb9
Ryan Lam <darth.ryking@gmail.com>
2024-03-09 16:25:38 -0500

    Tutorial stuff

--- game.js
+++ game.js
@@ -1,28 +1,25 @@
-class Example extends Phaser.Scene {
+class Game extends Phaser.Scene {
     preload() {
-        this.load.setBaseURL('https://labs.phaser.io');
-
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet(
+            'dude',
+            'assets/dude.png',
+            {
+                frameWidth: 32,
+                frameHeight: 48
+            }
+        );
     }
 
     create() {
         this.add.image(400, 300, 'sky');
+    }
 
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+    update() {
 
-        const logo = this.physics.add.image(400, 100, 'logo');
-
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        particles.startFollow(logo);
     }
 }
 
@@ -31,38 +28,6 @@ const game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: Example,
+    scene: Game,
     parent: 'game',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    }
 });
-
-
-const preload = () => {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet(
-        'dude',
-        'assets/dude.png',
-        {
-            frameWidth: 32,
-            frameHeight: 48
-        }
-    );
-};
-
-
-const create = () => {
-
-};
-
-
-const update = () => {
-
-};
