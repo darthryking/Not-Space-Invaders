e03ec0f88aee6e53bec5806858adf76c4383c93a
Ryan Lam <rwl@alumni.cmu.edu>
2024-03-12 22:08:01 -0400

    Implement MissileLauncher weapon

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 export const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const dist = (x1, y1, x2, y2) => {
+    const dX = x2 - x1;
+    const dY = y2 - y1;
+    return Math.sqrt(dX * dX + dY * dY);
+};
